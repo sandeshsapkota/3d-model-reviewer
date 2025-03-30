87bfbd7f6bc4e2e62ca8f7be52a42d4ddc34e3c8
Sandesh Sapkota <sapkotasandesh88@gmail.com>
2025-03-30 20:44:42 +0545

    navigation implementation and new 3d models

--- src/ModelsReviewer/context/CommentContext.tsx
+++ src/ModelsReviewer/context/CommentContext.tsx
@@ -1,15 +1,30 @@
-import {createContext, ReactNode, useContext, useRef, useState} from "react";
+import {createContext, ReactNode, useContext, useRef, useState, RefObject} from "react";
 import styles from "@/test.module.scss";
 import CommentSidebar from "@/ModelsReviewer/ModelSidebar/ModelSidebar.tsx";
 
-const CommentContext = createContext<any>(null);
+interface CommentContextType {
+    comment: string;
+    isCommentActive: boolean;
+    savedComments: Comment[];
+    activeVertices: Vertex;
+    handleToggleCommentMode: () => void;
+    handleSaveComment: (comment: string) => void;
+    setComment: (comment: string) => void;
+    handleToggleActive: (id: number) => void
+    utilsRef: RefObject<UtilsRefType>
+    setSavedComments: (comments: Comment[]) => void
+    setIsCommentActive: (active: boolean) => void
+    setActiveVertices: (vertices: Vertex) => void
+}
+
+const CommentContext = createContext<CommentContextType | null>(null);
 
 import {Comment, Vertex} from "@/@types";
 
-import {FaCaretLeft, FaCaretRight} from "react-icons/fa";
 import cx from "classnames";
 import {useFullScreen} from "@/ModelsReviewer/context/FullScreenContext.tsx";
 import CommentButton from "@/ModelsReviewer/ModelSidebar/CommentButton.tsx";
+import NextPrev from "@/ModelsReviewer/context/NextPrev.tsx";
 
 interface UtilsRefType {
     handleRotateCamera?: (vertices: Vertex) => void;
@@ -21,11 +36,10 @@ export const CommentProvider = ({children}: { children: ReactNode }) => {
     const [isCommentActive, setIsCommentActive] = useState(false);
     const [savedComments, setSavedComments] = useState<Comment[]>([]);
     const [activeVertices, setActiveVertices] = useState<Vertex>({x: null, y: null, z: null});
-    const {isFullScreen, toggleFullScreen} = useFullScreen()
+    const {isFullScreen} = useFullScreen()
 
     const handleToggleCommentMode = () => {
         setIsCommentActive(!isCommentActive);
-        toggleFullScreen()
     };
 
     const handleSaveComment = (comment: string) => {
@@ -76,17 +90,7 @@ export const CommentProvider = ({children}: { children: ReactNode }) => {
 
                 <div className={styles.modelArea}>
                     <CommentButton/>
-                    <div className="absolute top-4 right-4 flex gap-1.5 z-10">
-                        <button
-                            className="w-8 h-8 flex items-center justify-center bg-white border-none rounded  hover:bg-gray-100 cursor-pointer transition-colors">
-                            <FaCaretLeft size={16}/>
-                        </button>
-                        <button
-                            className="w-8 h-8 flex items-center justify-center bg-white border-none rounded  hover:bg-gray-100 cursor-pointer transition-colors">
-                            <FaCaretRight size={16}/>
-                        </button>
-
-                    </div>
+                    <NextPrev/>
                     {children}
 
                 </div>
